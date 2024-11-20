8b9b09d91575b960cb598f87aa8c96025625d522
Ravi Kumar <ravikumarakula55@gmail.com>
2024-11-20 21:29:18 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 apt update -y
+#JDK
 apt install default-jdk -y
 apt install unzip -y
+#MAVEN
 apt install maven -y
+#JENKINES
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
     https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
  echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
@@ -10,31 +13,43 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
  sudo apt-get update -y
  sudo apt-get install jenkins
+ # AWS CLI
  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+# TERRAFORM INSTALL
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 sudo apt-get install wget apt-transport-https gnupg lsb-release
-
+# TRIVY
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
 
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 
 sudo apt-get update
-
 sudo apt-get install trivy
 sudo apt update
+# DOCKER INSTALL
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
 sudo systemctl status docker
+# eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+# kubectl
 curl -LO https://dl.k8s.io/release/v1.21.14/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
+# creating the eks cluster and node group command 
+eksctl create cluster --name Three-tire-k8s-EKS-Cluster --region us-east-1 --node-typet2.micro --nodes-min 2--nodes-max 2 aws eks update-kubeconfig --region us-east-1 --name Three-Tier-K8s-EKS-Cluster
+
+
+
+
+
+
